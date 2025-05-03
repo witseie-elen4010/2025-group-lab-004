fb9edffb12d52983f0cdfd2decd91c3543ee56e1
AlungileRawuka <alungilerawuka@gmail.com>
2025-05-03 21:05:07 +0200

    Add game schema with code to join game

---
+++ src/models/Game.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const gameSchema = new mongoose.Schema({
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+    length: 6
+  },
+  players: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Game', gameSchema)
+
