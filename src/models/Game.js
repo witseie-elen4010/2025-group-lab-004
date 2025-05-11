0390b11f9903c498440717c5cf5afab42cc6c46a
Erick Chauke <erickchauke0217@gmail.com>
2025-05-11 08:45:22 +0200

    Player Voting and Elimination

--- src/models/Game.js
+++ src/models/Game.js
@@ -10,9 +10,45 @@ const gameSchema = new mongoose.Schema({
     length: 6
   },
   players: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    username: String,
+    role: {
+      type: String,
+      enum: ['civilian', 'undercover', 'mrwhite'],
+      default: 'civilian'
+    },
+    word: String,
+    isEliminated: {
+      type: Boolean,
+      default: false
+    },
+    eliminatedInRound: Number
   }],
+  currentRound: {
+    type: Number,
+    default: 1
+  },
+  status: {
+    type: String,
+    enum: ['waiting', 'in-progress', 'completed'],
+    default: 'waiting'
+  },
+  votes: [{
+    round: Number,
+    voterId: mongoose.Schema.Types.ObjectId,
+    votedForId: mongoose.Schema.Types.ObjectId
+  }],
+  eliminatedPlayers: [{
+    userId: mongoose.Schema.Types.ObjectId,
+    username: String,
+    role: String,
+    round: Number
+  }],
+  civilianWord: String,
+  undercoverWord: String,
   createdAt: {
     type: Date,
     default: Date.now
@@ -21,4 +57,3 @@ const gameSchema = new mongoose.Schema({
 
 const Game = mongoose.model('Game', gameSchema)
 module.exports = Game
-
